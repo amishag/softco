4311139058797875e74c5c140d06c36325be3aa9
Amisha <21112000amisha@gmail.com>
2020-06-16 23:57:24 +0530

    first commit

---
+++ home1.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>SoftCoPlus</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+<link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+</head>
+
+<body>
+	<div class="header" id="topheader">
+<nav class="navbar navbar-expand-lg navbar-light bg-white">
+	<ul class="navbar-nav ml-auto ">
+               <div class="uppernav">
+					<a href="#"><i class="fa fa-search" aria-hidden="true"></i></a>
+					<span>000800006000</span>
+					<span>contact us</span>
+					<a href="#"><i class="fa fa-phone" aria-hidden="true"></i></a>
+					<span>login</span>
+					<a href="#"><i class="fa fa-sign-in" aria-hidden="true"></i></a>
+					
+				</div>
+			</ul>
+			</nav>
+				
+	<nav class="navbar navbar-expand-lg navbar-light bg-white">
+
+				
+  <a class="navbar-brand font-weight-bold text-maroon" href="#">SOFTCOPLUS</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+				
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto ">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">PRODUCTS<span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Solutions</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">support and service</a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link" href="#">events</a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link" href="#">resources</a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link" href="#">About us</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Covid 19 </a>
+      </li>
+      
+    </ul>
+  </div>
+
+  <div>
+  	<button class="register">TRY FOR FREE</button>
+  </div>
+</nav>
+
+
+
+<section class="header-section">
+<div class="center-div">
+	<h2 class="font-weight-bold">An application development company with skilled software developers</h2><p>SoftCoPlus is a Canada based software development company</p><br>
+	<div class="header-button">
+	<a href="#">GET STARTED</a>
+	</div>
+</div>  
+</section>
+</div>
+<!---header finish--->
+<!----three extra header div starts-->
+<section class="header-extradiv">
+	<div class="container">
+		<div class="row">
+			<div class="extradiv col-lg-4 col-md-4 col-12">
+				<a href="#"><i class="fa-3x fa fa-desktop" aria-hidden="true"></i></a>
+				<h2>APP DEVELOPMENT</h2>
+				<p><font color="black"> Our team of software developers can build exceptional applications across a breadth of devices including desktop, mobile and tablet. With the help of our industry expertise, we are able to give the end user the best application experience across multiple channels.</font></p>
+			</div>
+			<div class="extradiv col-lg-4 col-md-4 col-12">
+				<a href="#"><i class="fa-3x fa fa-users" aria-hidden="true"></i></a>
+				<h2>On-Demand Developers</h2>
+				<p><font color="black">We are proud to employ over 1,800 in-house developers with a variety of software development experience in an array of industries. This enables us to be a one-stop shop, removing the hassles of managing multiple vendors, logistics, and technological bottlenecks.</font></p>
+			</div>
+			<div class="extradiv col-lg-4 col-md-4 col-12">
+				<a href="#"><i class="fa-3x fa fa-life-ring" aria-hidden="true"></i></a>
+				<h2>Product Support</h2>
+				<p><font color="black">We partner with other industry leaders to deploy superior software solutions for our clients. Our certified developers are capable of implementing new software, seamlessly integrating it to your organization as well as providing support for any future upgrades.</font></p>
+			</div>
+			
+		</div>
+		
+	</div>
+	
+</section>
+
+
+</body>
+</html>
